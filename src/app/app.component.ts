9b57508ece8b2aae5720a33ecce76024ccf2aae4
Flemming Bregnvig <flemming@nestech.dk>
2024-02-28 18:30:12 +0100

    Moved files til "lib"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true
+  selector: 'app-root',
+  template: `
+  <main class="vh-100 d-flex justify-content-center align-items-center">
+    <div class="d-relative">
+      <img src="assets/angular.png" height="400">
+    </div>
+  </main>
+  `,
+  standalone: true
 })
 export class AppComponent {
 }
